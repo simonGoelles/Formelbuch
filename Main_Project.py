43bf6de93c5faf6caeb4a011c0d338ef5a706713
simonGoelles <goelle190028@sr.htlweiz.at>
2023-01-26 17:30:53 +0100

    still_not_quite_done

--- Main_Project.py
+++ Main_Project.py
@@ -1,1 +1,13 @@
 # this is a suprise tool that will help us later
+from Crude_experimental import create, read, update, delete
+
+
+if __name__ == '__main__':
+    print("--- create() ---")
+    id = create()
+    print("--- read() ---")
+    read(id)
+    print("--- update() ---")
+    update(id)
+    print("--- delete() ---")
+    delete(id)
